34c5c2551cc0da7e5bf0b8131b69e0ba887c23b6
Harshal <harshalnrn>
2019-07-19 09:08:08 +0530

    dynamically render data by iterating through array of objects

--- src/App.js
+++ src/App.js
@@ -6,6 +6,21 @@ import './App.css';
 class App extends Component{
 
   render(){
+    let subsriberArray=[
+
+      {
+        name:'Harshal',
+      phone:'494844494'
+    },
+      {
+        name:'Shilpa',
+        phone:'6464646464'
+      },
+      {
+          name:'Varun',
+        phone:'464989844'
+      }
+    ]
   return (
     <div>
     <Header />
@@ -16,6 +31,15 @@ class App extends Component{
         <span className="grid-item name-heading">Name</span>
         <span className="grid-item phone-heading">Phone</span>
       </div>
+      {
+subsriberArray.map(sub=>{
+return <div className="grid-container">
+  <span className="grid-item">{sub.name}</span>
+  <span className="grid-item">{sub.phone}</span>
+  </div>
+
+})
+      } 
     </div>
     </div>
  );
