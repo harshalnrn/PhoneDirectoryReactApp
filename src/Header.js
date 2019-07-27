607dac62732b0e002e95fd754a0739f084df6eab
Harshal <harshalnrn>
2019-07-27 08:30:12 +0530

    Passed heading to Header component as props

--- src/Header.js
+++ src/Header.js
@@ -3,27 +3,28 @@ import './Header.css' ;
 //components have to mandatorily return react element
 
 //note the difference in css properties syntax in JSX as compared to raw css. in JSX, CSS properties are perceived as JS properties
-/* const headerStyle = {
+/*  const headerStyle = {
     textAlign: 'center',
     padding: 20,
     background: '#000',
     color: '#fff',
     textTransform: 'uppercase'
-  }; 
- const Header=function(){                                  //constant name should be same as that of component.js
+  };  */
+/*  const Header=function(props){                                  //constant name should be same as that of component.js
 return(  
 <div className='header'>
-Phone Directory
+{props.heading}
  </div>
  )
-} */ 
+}  
+*/
 //now reference this functional component in App.js
 //below is the class component
- class Header extends Component{
+  class Header extends Component{
 
     render(){
 
         return(
-            <div className="header">Phone Directory</div>         )
-     } }
+            <div className="header">{this.props.heading}</div>  )
+     } } 
 export default Header;
